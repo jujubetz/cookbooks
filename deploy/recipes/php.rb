297fb6a8841fcf24e8985b98b32fb18a06b41fe8
Juliano <juliano@oozo.tv>
2016-01-06 15:09:31 -0200

    user/group added
    1

--- deploy/recipes/php.rb
+++ deploy/recipes/php.rb
@@ -14,9 +14,9 @@ node[:deploy].each do |application, deploy|
   end
 
   opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
+    user deploy[:ec2-user]
+    group deploy[:ec2-user]
+    path deploy[:/home/ec2-user/oozo]
   end
 
   opsworks_deploy do
