45f79a6869887790c9c05b253c66718f9aab1aaf
Juliano <juliano@oozo.tv>
2016-01-08 09:42:19 -0200

    repair mysql compatibility problems with php app deploy

--- mysql/metadata.rb
+++ mysql/metadata.rb
@@ -1,24 +1,10 @@
-name 'mysql'
-maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
-description 'Provides mysql_service, mysql_config, and mysql_client resources'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '6.1.2'
+name        "mysql"
+description "Installs and configures MySQL"
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
 
-supports 'amazon'
-supports 'redhat'
-supports 'centos'
-supports 'scientific'
-supports 'fedora'
-supports 'debian'
-supports 'ubuntu'
-supports 'smartos'
-supports 'omnios'
-supports 'suse'
+depends "opsworks_commons"
 
-depends 'yum-mysql-community'
-depends 'smf'
-
-source_url 'https://github.com/chef-cookbooks/mysql' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/mysql/issues' if respond_to?(:issues_url)
+recipe "mysql::client", "Installs MySQL"
+recipe "mysql::server", "Installs MySQL"
