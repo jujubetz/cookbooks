ddeea26a104d3548a3357cd059f76791d1048e0f
Juliano <juliano@oozo.tv>
2016-01-12 17:37:07 -0200

    browscap

--- browscap/recipes/default.rb
+++ browscap/recipes/default.rb
@@ -1,5 +1,5 @@
-remote_file '/etc/php.d/' do
-  source 'http://browscap.org/stream?q=Lite_PHP_BrowsCapINI'
+remote_file '/etc/php.d/browscap.ini' do
+  source 'https://www.dropbox.com/s/hzdfgkhwf6t5ub4/browscap.ini?dl=1'
   owner 'root'
   group 'root'
   mode '0755'
