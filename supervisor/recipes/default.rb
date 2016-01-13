14d5142c9b973aec6f772ba6a2fe2d00c8d5c2cf
Juliano <juliano@oozo.tv>
2016-01-13 18:54:09 -0200

    rollback #112

--- supervisor/recipes/default.rb
+++ supervisor/recipes/default.rb
@@ -1,121 +1,71 @@
-#
-# Cookbook Name:: supervisor
-# Recipe:: default
-#
-# Copyright 2011, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-include_recipe "python"
-
-# foodcritic FC023: we prefer not having the resource on non-smartos
-if platform_family?("smartos")
-  package "py27-expat" do
-    action :install
-  end
+script "Install supervisor" do
+    interpreter "bash"
+    user "root"
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOH
+    pip install supervisor
+    EOH
+    #update-rc.d supervisord defaults
+    # not_if "which supervisorctl"
 end
 
-python_pip "supervisor" do
-  action :upgrade
-  version node['supervisor']['version'] if node['supervisor']['version']
-end
-
-directory node['supervisor']['dir'] do
-  owner "root"
-  group "root"
-  mode "755"
-  recursive true
-end
-
-template node['supervisor']['conffile'] do
-  source "supervisord.conf.erb"
-  owner "root"
-  group "root"
-  mode "644"
-  variables({
-    :inet_port => node['supervisor']['inet_port'],
-    :inet_username => node['supervisor']['inet_username'],
-    :inet_password => node['supervisor']['inet_password'],
-    :supervisord_minfds => node['supervisor']['minfds'],
-    :supervisord_minprocs => node['supervisor']['minprocs'],
-    :supervisor_version => node['supervisor']['version'],
-    :socket_file => node['supervisor']['socket_file'],
-  })
-end
-
-directory node['supervisor']['log_dir'] do
-  owner "root"
-  group "root"
-  mode "755"
-  recursive true
-end
-
-template "/etc/default/supervisor" do
-  source "debian/supervisor.default.erb"
-  owner "root"
-  group "root"
-  mode "644"
-  only_if { platform_family?("debian") }
-end
-
-init_template_dir = value_for_platform_family(
-  ["rhel", "fedora", "centos", "amazon"] => "rhel",
-  "debian" => "debian"
-)
-
-prefix_dir = node['python']['prefix_dir']
-prefix_dir += '/local' if node['platform'] == 'amazon'
-
-case node['platform']
-when "amazon", "centos", "debian", "fedora", "redhat", "ubuntu", "raspbian"
-  template "/etc/init.d/supervisor" do
-    source "#{init_template_dir}/supervisor.init.erb"
+directory "/etc/supervisor/" do
     owner "root"
     group "root"
-    mode "755"
-    variables({
-      # TODO: use this variable in the debian platform-family template
-      # instead of altering the PATH and calling "which supervisord".
-      :supervisord => "#{prefix_dir}/bin/supervisord"
-    })
-  end
+    mode "0755"
+end
 
-  service "supervisor" do
-    supports :status => true, :restart => true
-    action [:enable, :start]
-  end
-when "smartos"
-  directory "/opt/local/share/smf/supervisord" do
+case node.platform
+when "redhat", "centos", "fedora"
+    cookbook_file "/etc/init.d/supervisord" do
+        source "etc/init.d/redhat.supervisord"
+        owner "root"
+        group "root"
+        mode "0755"
+    end
+    # chkconfig --add supervisord
+    # chkconfig supervisord on
+when "debian", "ubuntu"
+    cookbook_file "/etc/init.d/supervisord" do
+        source "etc/init.d/debian.supervisord"
+        owner "root"
+        group "root"
+        mode "0755"
+    end
+    # sudo update-rc.d supervisord defaults
+end
+
+service "supervisord" do
+    supports :restart => true, :reload => true, :status => true
+    action :enable
+end
+
+node[:supervisor][:includes].each do |inc|
+    template "/etc/supervisor/#{inc}.conf" do
+        source "etc/supervisor/#{inc}.conf.erb"
+        owner "root"
+        group "root"
+        mode "0644"
+        cookbook node[:supervisor].attribute?("config_cookbook") ? node[:supervisor][:config_cookbook] : "supervisor"
+        notifies :restart, resources(:service => "supervisord")
+    end
+end
+
+template "/etc/supervisord.conf" do
+    source "etc/supervisord.conf.erb"
     owner "root"
     group "root"
-    mode "755"
-  end
+    mode "0644"
+    notifies :restart, resources(:service => "supervisord")
+end
 
-  template "/opt/local/share/smf/supervisord/manifest.xml" do
-    source "manifest.xml.erb"
-    owner "root"
-    group "root"
-    mode "644"
-    notifies :run, "execute[svccfg-import-supervisord]", :immediately
-  end
-
-  execute "svccfg-import-supervisord" do
-    command "svccfg import /opt/local/share/smf/supervisord/manifest.xml"
-    action :nothing
-  end
-
-  service "supervisord" do
-    action [:enable]
-  end
+case node.platform
+when "redhat", "centos", "fedora"
+    execute "sudo /sbin/service supervisord start" do
+        not_if "pgrep -f supervisord"
+    end
+when "debian", "ubuntu"
+    execute "sudo /etc/init.d/supervisord start" do
+        not_if "pgrep -f supervisord"
+    end
 end
