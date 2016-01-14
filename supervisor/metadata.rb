e9475a1a9f048beadc16da96ca260cb7cf7430aa
Juliano <juliano@oozo.tv>
2016-01-14 10:27:38 -0200

    supervisor~> newt

--- supervisor/metadata.rb
+++ supervisor/metadata.rb
@@ -1,13 +1,14 @@
-maintainer        "David Marble"
-maintainer_email  "davidmarble@gmail.com"
-license           "Apache 2.0"
 name              "supervisor"
-description       "supervisor recipes"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+maintainer        "Noah Kantrowitz"
+maintainer_email  "noah@coderanger.net"
+license           "Apache 2.0"
+description       "Installs supervisor and provides resources to configure services"
+version           "0.4.12"
 
-recipe            "supervisor", ""
-    
-%w{ debian ubuntu centos redhat }.each do |os|
+recipe "supervisor", "Installs and configures supervisord"
+
+depends "python"
+
+%w{ ubuntu debian redhat centos fedora amazon smartos raspbian }.each do |os|
   supports os
 end
